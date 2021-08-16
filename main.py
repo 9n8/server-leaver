09b56e6a0f0eb3a7d70eac8f0ae9df156d1cabcc
pw#1337 <87565211+herluvhr@users.noreply.github.com>
2021-08-16 13:29:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests, threading, os
+from colorama import Fore
+
+os.system(f'title [serverl leaver]')
+os.system(f'mode 80,20')
+
+print(f'                                 {Fore.CYAN}server leaver \n\n')
+
+print(f'                                   {Fore.YELLOW}@pw1337 {Fore.LIGHTMAGENTA_EX} \n')
+
+def leave(guild_id, token):
+   rq = requests.delete(f"https://discord.com/api/v9/users/@me/guilds/{guild_id}", headers={"Authorization": token})
+   if rq.status_code == 204 or 200:
+     print(f"left {guild_id}")
+def get_all_guilds(token) -> list():
+   servers = []
+   rq = requests.get("https://discord.com/api/v9/users/@me/guilds", headers={"Authorization": token})
+   for guild in rq.json():
+    servers.append(guild["id"])
+   return servers
+def start():
+   token = input(f"Token: ")
+   guilds = get_all_guilds(token)
+   for guild in guilds:
+       threading.Thread(target=leave, args=(guild, token,)).start()
+start()
